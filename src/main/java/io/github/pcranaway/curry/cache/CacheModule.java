6f5d4b5e98b03604548ed93c97ba45c4fb0baa1a
pcranaway <pcrunn.alex@gmail.com>
2020-06-24 13:43:01 +0300

    fix imports

--- src/main/java/io/github/pcranaway/curry/cache/CacheModule.java
+++ src/main/java/io/github/pcranaway/curry/cache/CacheModule.java
@@ -1,8 +1,8 @@
 package io.github.pcranaway.curry.cache;
 
 import io.github.pcranaway.curry.cache.type.CacheType;
-import io.github.pcranaway.curry.cache.type.CacheTypeProvider;
-import io.github.pcranaway.curry.cache.type.SimpleCacheTypeProvider;
+import io.github.pcranaway.curry.cache.type.provider.CacheTypeProvider;
+import io.github.pcranaway.curry.cache.type.provider.SimpleCacheTypeProvider;
 
 import java.util.List;
 
