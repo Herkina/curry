0214760ffb0890a173f27eb7e367f4c9c3fdf85f
pcranaway <pcrunn.alex@gmail.com>
2020-06-24 13:48:11 +0300

    comment more code

--- src/main/java/io/github/pcranaway/curry/cache/type/provider/CacheTypeProvider.java
+++ src/main/java/io/github/pcranaway/curry/cache/type/provider/CacheTypeProvider.java
@@ -2,10 +2,24 @@ package io.github.pcranaway.curry.cache.type.provider;
 
 import io.github.pcranaway.curry.cache.type.CacheType;
 
+/**
+ * Provides methods for interacting with the {@link CacheType}
+ */
 public interface CacheTypeProvider {
 
+    /**
+     * Finds a {@link CacheType}
+     *
+     * @param name the name of the {@link CacheType}
+     * @return the {@link CacheType}
+     */
     CacheType find(String name);
 
+    /**
+     * Registers a {@link CacheType}
+     *
+     * @param cacheType the {@link CacheType}
+     */
     void register(CacheType cacheType);
 
 }
