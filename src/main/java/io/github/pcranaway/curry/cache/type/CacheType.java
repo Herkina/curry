abd616cc6e9df697628beabd030586058a7030be
pcranaway <pcrunn.alex@gmail.com>
2020-06-24 13:44:27 +0300

    functions -> methods

--- src/main/java/io/github/pcranaway/curry/cache/type/CacheType.java
+++ src/main/java/io/github/pcranaway/curry/cache/type/CacheType.java
@@ -1,7 +1,15 @@
 package io.github.pcranaway.curry.cache.type;
 
+/**
+ * A cache type providing all methods for interacting with the cache
+ */
 public interface CacheType {
 
+    /**
+     * Gets the name of the {@link CacheType}
+     *
+     * @return the name
+     */
     String getName();
 
 }
