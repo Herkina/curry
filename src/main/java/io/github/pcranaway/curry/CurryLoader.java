d703971582bec13d6c5318e5c719b9a3591a95b0
pcranaway <pcrunn.alex@gmail.com>
2020-06-24 13:12:21 +0300

    :tada:

---
+++ src/main/java/io/github/pcranaway/curry/CurryLoader.java
@@ -0,0 +1,12 @@
+package io.github.pcranaway.curry;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CurryLoader extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
