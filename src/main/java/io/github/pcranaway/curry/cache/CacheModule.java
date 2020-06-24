14ce4c5e9853ffa89f38d73fb3e973dc4b3c1aee
pcranaway <pcrunn.alex@gmail.com>
2020-06-24 13:45:48 +0300

    comment code

--- src/main/java/io/github/pcranaway/curry/cache/CacheModule.java
+++ src/main/java/io/github/pcranaway/curry/cache/CacheModule.java
@@ -6,10 +6,18 @@ import io.github.pcranaway.curry.cache.type.provider.SimpleCacheTypeProvider;
 
 import java.util.List;
 
+/**
+ * Initializes and handles the cache
+ */
 public class CacheModule {
 
     private final CacheTypeProvider cacheTypeProvider;
 
+    /**
+     * Creates the {@link CacheModule}
+     *
+     * @param cacheTypeList the list of {@link CacheType} to be registered
+     */
     public CacheModule(List<CacheType> cacheTypeList) {
         this.cacheTypeProvider = new SimpleCacheTypeProvider();
 
